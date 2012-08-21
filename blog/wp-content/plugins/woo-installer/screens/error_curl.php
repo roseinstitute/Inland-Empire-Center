1ca736a00eae22d82e7f2effc2d48ee49c1b5547
Dave Meyer <dmeyer@atlassian.com>
2012-08-21 14:04:05 -0700

    removed the blog

--- blog/wp-content/plugins/woo-installer/screens/error_curl.php
+++
@@ -1,3 +0,0 @@
-<div class="error fade">
-	<p><?php _e( 'We noticed that the `cURL` extension is not enabled on your server. Woo-Installer requires `cURL` in order to function. Please contact your system administrator and request that `cURL` be enabled.', 'woothemes' ); ?></p>
-</div><!--/.error fade-->
